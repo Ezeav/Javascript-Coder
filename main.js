d62372a4676600c0508e42331ada54aa09a00e16
Ezequiel <ezequielagustin044@gmail.com>
2025-06-30 01:24:27 -0300

    PRIMERA CLASE

---
+++ main.js
@@ -0,0 +1,23 @@
+let nombrePrompt = prompt("Ingrese su nombre:");
+let edad = 30;
+let profesion = "Ingeniero";
+let ciudad = "Miami";
+
+console.log(nombrePrompt);
+console.log(edad);
+console.log(profesion);
+console.log(ciudad);
+
+let x = 10;
+let y = 20;
+// Operadores aritméticos
+console.log(x + y); // Suma
+console.log(x - y); // Resta
+console.log(x * y); // Multiplicación
+console.log(x / y); // División
+console.log(x % y); // Módulo (resto de la división)
+console.log(x ** y); // Exponenciación (x elevado a la y)
+
+//booleanos
+console.log(x == y);
+console.log(x != y); // (es diferente o igual a y)
